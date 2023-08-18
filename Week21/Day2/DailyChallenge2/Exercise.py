547693f8b8dd1a354ab259f8b32eaa2b5d80536e
ehud <ehudmi12@gmail.com>
2023-08-18 10:52:16 +0300

    some more challenges

--- Week21/Day2/DailyChallenge2/Exercise.py
+++ Week21/Day2/DailyChallenge2/Exercise.py
@@ -5,3 +5,22 @@
 # Look at this result :
 # {"Bonjour": "Hello", "Au revoir": "Goodbye", "Bienvenue": "Welcome", "A bientôt": "See you soon"}
 # You have to recreate the result using a translator module. Take a look at the googletrans module
+
+import googletrans
+
+gt = googletrans.Translator()
+
+french_words = ["Bonjour", "Au revoir", "Bienvenue", "A bientôt"]
+translated = []
+
+
+def french_translate(words):
+    for word in words:
+        temp = gt.translate(word, "en", "fr")
+        # print(temp)
+        translated.append({word: temp.text})
+    return translated
+
+
+french_translate(french_words)
+print(translated)
--- Week21/Day2/MiniProject/Exercise.py
+++ Week21/Day2/MiniProject/Exercise.py
@@ -32,3 +32,78 @@
 # life points (eg if your attack is 20 and life is 5 you will subtract 4 life points).
 
 # Once all your classes are created start testing them, create one of each character and use each of their method.
+
+
+class Character:
+    def __init__(self, name, life=20, attack=10) -> None:
+        self.name = name
+        self.life = life
+        self.attack = attack
+
+    def basic_attack(self, character):
+        character.life = character.life - 1
+
+
+class Druid(Character):
+    def __init__(self, name, life=20, attack=10) -> None:
+        super().__init__(name, life, attack)
+        print(f"{self.name} is created by the ancients")
+
+    def meditate(self):
+        self.life += 10
+        self.attack -= 2
+        return self
+
+    def animal_help(self):
+        self.attack += 5
+
+    def fight(self, character):
+        character.life -= 0.75 * self.life + 0.75 * self.attack
+
+
+class Warrior(Character):
+    def __init__(self, name, life=20, attack=10) -> None:
+        super().__init__(name, life, attack)
+        print(f"{self.name} is trained by the barbarians")
+
+    def brawl(self, character):
+        character.life -= 2 * self.attack
+        self.life += 0.5 * self.attack
+
+    def train(self):
+        self.life += 2
+        self.attack += 2
+
+    def roar(self, character):
+        character.attack -= 3
+
+
+class Mage(Character):
+    def __init__(self, name, life=20, attack=10) -> None:
+        super().__init__(name, life, attack)
+        print(f"{self.name} is formed by the wizards")
+
+    def curse(self, character):
+        character.attack -= 2
+
+    def summon(self):
+        self.attack += 3
+
+    def cast_spell(self, character):
+        character.life -= int(self.attack / self.life)
+
+
+druid_1 = Druid("Merlin")
+warrior_1 = Warrior("Conan")
+mage_1 = Mage("Harry Potter")
+# druid_1.meditate()
+# druid_1.basic_attack(warrior_1)
+# druid_1.animal_help()
+# druid_1.fight(warrior_1)
+# print(druid_1.life, druid_1.attack)
+# print(warrior_1.life, warrior_1.attack)
+# warrior_1.brawl(druid_1)
+# warrior_1.roar(druid_1)
+# warrior_1.train()
+# print(druid_1.life, druid_1.attack)
+# print(warrior_1.life, warrior_1.attack)
