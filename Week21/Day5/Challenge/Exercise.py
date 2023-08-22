1d5f70e5c3afe5a46b4aa338eaa14484831c1595
ehud <ehudmi12@gmail.com>
2023-08-22 17:53:31 +0300

    week 21 day 5 exercise

---
+++ Week21/Day5/Challenge/Exercise.py
@@ -0,0 +1,5 @@
+# Instructions
+# Using this REST Countries API, https://restcountries.com/ create the functionality which will write 10 random
+# countries to your database.
+
+# These are the attributes which you should populate your tables with: name, capital, flag, subregion, population.
