d50e0fdcbcdb97d7bfe82ebb68b2132e37496960
ehud <ehudmi12@gmail.com>
2023-09-08 15:22:21 +0300

    moving forward with the hackathon/final

--- fight_game/fight/urls.py
+++ fight_game/fight/urls.py
@@ -6,4 +6,12 @@ urlpatterns = [
     path("signup/", views.SignUpView.as_view(), name="signup"),
     path("get_figure/", views.get_figure, name="get-figure"),
     path("get_image/", views.get_image, name="get-image"),
+    path("search_figure/", views.search_figure, name="search-figure"),
+    path("select_figure/", views.select_figure, name="select-figure"),
+    path(
+        "select_image/",
+        views.select_image,
+        name="select-image",
+    ),
+    # path("battle_screen/",views.)
 ]
