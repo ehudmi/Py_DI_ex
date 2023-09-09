f1d6286189656bf4046586c9f24b9526163d684c
ehud <ehudmi12@gmail.com>
2023-09-09 21:57:23 +0300

    added crispy - not styled yet

--- fight_game/config/settings.py
+++ fight_game/config/settings.py
@@ -39,10 +39,16 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # 3rd party
+    "crispy_forms",
+    "crispy_bootstrap5",
     "bootstrap5",
     # Local Apps
     "fight",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 AUTH_USER_MODEL = "fight.FightUser"
 
