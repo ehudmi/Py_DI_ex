b343b25ccdb7f202350f408fdd19c813221063d0
ehud <ehudmi12@gmail.com>
2023-09-18 10:33:11 +0300

    IMDI gold

--- Week24/Day2/FilmProject/FilmProject/films/urls.py
+++ Week24/Day2/FilmProject/FilmProject/films/urls.py
@@ -4,17 +4,27 @@ from . import views
 urlpatterns = [
     path(
         "films/homepage",
-        views.HomePageView.as_view(template_name="films/homepage.html"),
+        views.HomePageView.as_view(),
         name="homepage",
     ),
     path(
         "films/addFilm",
-        views.FilmCreateView.as_view(template_name="film/addFilm.html"),
+        views.FilmCreateView.as_view(),
         name="add-film",
     ),
     path(
         "director/addDirector",
-        views.DirectorCreateView.as_view(template_name="director/addDirector.html"),
+        views.DirectorCreateView.as_view(),
         name="add-director",
+    ),
+    path(
+        "films/editFilm/<pk>",
+        views.FilmUpdateView.as_view(),
+        name="edit-film",
+    ),
+    path(
+        "director/editDirector/<pk>",
+        views.DirectorUpdateView.as_view(),
+        name="edit-director",
     ),
 ]
--- Week24/Day2/FilmProject/FilmProject/films/views.py
+++ Week24/Day2/FilmProject/FilmProject/films/views.py
@@ -1,20 +1,37 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from .models import Film
+from .models import Film, Director
 from .forms import FilmForm, DirectorForm
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 
 
 class HomePageView(ListView):
     model = Film
+    template_name = "films/homepage.html"
     context_object_name = "films"
 
 
 class FilmCreateView(CreateView):
     form_class = FilmForm
+    template_name = "film/addFilm.html"
     success_url = reverse_lazy("homepage")
 
 
 class DirectorCreateView(CreateView):
     form_class = DirectorForm
+    template_name = "director/addDirector.html"
+    success_url = reverse_lazy("homepage")
+
+
+class FilmUpdateView(UpdateView):
+    model = Film
+    form_class = FilmForm
+    template_name = "film/editFilm.html"
+    success_url = reverse_lazy("homepage")
+
+
+class DirectorUpdateView(UpdateView):
+    model = Director
+    form_class = DirectorForm
+    template_name = "director/editDirector.html"
     success_url = reverse_lazy("homepage")
