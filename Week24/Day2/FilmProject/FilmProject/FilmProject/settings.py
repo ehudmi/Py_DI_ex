3de4f6ec58e47bb88c783cc0bd1407e9203433c8
ehud <ehudmi12@gmail.com>
2023-09-24 10:39:45 +0300

    week 24 day 4 xp

--- Week24/Day2/FilmProject/FilmProject/FilmProject/settings.py
+++ Week24/Day2/FilmProject/FilmProject/FilmProject/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "crispy_bootstrap5",
     # local apps
     "films",
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -136,3 +137,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+LOGIN_REDIRECT_URL = "homepage"
--- Week24/Day2/FilmProject/FilmProject/accounts/views.py
+++ Week24/Day2/FilmProject/FilmProject/accounts/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+# from django.views.generic import CreateView, DetailView
+from django.urls import reverse_lazy
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
 
-# Create your views here.
+# from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.models import User
+from .forms import SignUpForm, LoginForm
+
+
+# class SignupView(CreateView):
+#     form_class = SignUpForm
+#     success_url = reverse_lazy("login")
+#     template_name = "accounts/register.html"
+def signup_view(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        user = authenticate(
+            request,
+            username=request.POST.get("username"),
+            password=request.POST.get("password"),
+        )
+        if user is not None:
+            login(request, user)
+        else:
+            user = form.save()
+    else:
+        form = SignUpForm()
+    context = {"form": form}
+    return render(request, "accounts/register.html", context)
+
+
+# class UserLoginView(LoginView):
+#     form_class = LoginForm
+#     template_name = "accounts/login.html"
+def log_in_view(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        user = authenticate(
+            request,
+            username=request.POST.get("username"),
+            password=request.POST.get("password"),
+        )
+        if user is not None:
+            login(request, user)
+            return redirect("homepage")
+        else:
+            return redirect("signup")
+    else:
+        form = LoginForm()
+    context = {"form": form}
+    return render(request, "accounts/login.html", context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("homepage")
+
+
+def profile(request, user_id):
+    user = User.objects.get(id=user_id)
+    context = {"user": user}
+    return render(request, "accounts/profile.html", context)
