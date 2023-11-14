228caaad6b08dbcf2e4e7c33ca405dd99334b1b4
ehud <ehudmi12@gmail.com>
2023-11-14 17:50:19 +0200

    moved fight_game

--- fight_game/fight/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class FightConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "fight"
-
-    def ready(self):
-        import fight.signals
